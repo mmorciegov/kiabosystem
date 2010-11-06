f0b28cb3406ceed54c74ab16957807d18b525a56
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-06 13:16:54 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

---
+++ trunk/app/controllers/Vendas/Produto.php
@@ -0,0 +1,97 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Produto
+ *
+ * @author Luan
+ */
+class Produto {
+    private $codigo;
+    private $nome;
+    private $valor;
+    private $categoria;
+
+     /** Construtor - define o codigo do produto
+     *
+     * @param <String> $codigo
+     * @param <String> $nome
+      * @param <float> $valor
+      * @param <Categoria> $categoria
+     */
+    public function  __construct($codigo, $nome, $valor, $categoria) {
+
+    }
+
+     /** Retorna o codigo do produto
+     *
+     * @return <String> $codigo
+     */
+    public function getCodigo(){
+
+    }
+
+     /** Define o novo codigo do produto
+     *
+     * @param <String> $codigo - Codigo do produto
+     * @return void
+     */
+    public function setCodigo($codigo){
+
+    }
+
+     /** Retorna o nome do produto
+     *
+     * @return <String> $nome
+     */
+    public function getNome(){
+
+    }
+
+     /** Define o novo nome do produto
+     *
+     * @param <String> $nome - nome do produto
+     * @return void
+     */
+    public function setTelefone($nome){
+
+    }
+
+     /** Retorna o valor do produto
+     *
+     * @return <float> $valor
+     */
+    public function getValor(){
+
+    }
+
+     /** Define o novo valor do produto
+     *
+     * @param <float> $valor - valor do produto
+     * @return void
+     */
+    public function setValor($valor){
+
+    }
+
+     /** Retorna a categoria referente ao produto
+     *
+     * @return <Categoria> $categoria
+     */
+    public function getCategoria(){
+
+    }
+
+     /** Define a nova categoria referente ao produto
+     *
+     * @param <Categoria> $categoria - categoria referente ao produto
+     * @return void
+     */
+    public function setCategoria($categoria){
+
+    }
+}
+?>

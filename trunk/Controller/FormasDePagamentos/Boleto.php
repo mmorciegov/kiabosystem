f87b1ff1ae00b07f14b538348923a8edc3eef08b
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-06 11:52:25 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

---
+++ trunk/Controller/FormasDePagamentos/Boleto.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * Classe Boleto - Responsavel por definir pagamentos realizados por boletos bancarios.
+ * Ela eh uma subclasse de FormaPagamento.
+ */
+
+/**
+ * Descrição da Classe Boleto
+ *
+ * @author Rafael Tosta Santos <rafa.ecomp@gmail.com>
+ * @date 01/11/2010
+ * @version 1.0
+ * @group UEFS - Engenharia da Computacao
+ * @see Projeto de Graduação para a disciplina de Engenharia de Software 2010.2
+ * @link Repositorio <http://kiabosystem.googlecode.com/svn/trunk/Model/FormasDePagamento>
+ * @ref Boleto
+ */
+class Boleto extends FormaPagamento {
+    private $nomeDoCliente; /// Variavel que armazena o nome do cliente
+    private $data; /// Variavel que armazena a data da compra
+    private $vencimento; /// Variavel que armazena o vencimento de vencimento do boleto
+
+
+    /** Construtor - inicia as variaveis com os valores disponibilizados
+     *
+     * @param <int> $codigo
+     * @param <String> $nomeDoCliente
+     * @param <String> $data
+     * @param <String> $vencimento
+     */
+    public function   __construct($codigo, $nomeDoCliente, $data, $vencimento) {
+        parent::__construct($codigo);
+    }
+
+    /** Retorna o nome do cliente
+     *
+     * @return <String> $nomeDoCliente
+     */
+    public function getNome() {
+
+    }
+
+    /** Define novo nome do cliente
+     *
+     * @param <String> $novoNome
+     */
+    public function setNome($novoNome) {
+
+    }
+
+    /** Retorna a data do pagamento
+     *
+     * @return <String> $data
+     */
+    public function getData() {
+
+    }
+
+    /** Define nova data de pagamento
+     *
+     * @param <String> $novaData
+     */
+    public function setData($novaData) {
+
+    }
+
+    /** Retorna o vencimento do pagamento
+     *
+     * @return <String> $vencimento
+     */
+    public function getVencimento() {
+
+    }
+
+    /** Define novo vencimento do pagamento
+     *
+     * @param <String> $novoVencimento
+     */
+    public function setVencimento($novoVencimento) {
+
+    }
+
+
+}
+?>

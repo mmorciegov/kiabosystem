d0190fb0614b2ed55ee28d6264630e2be730ff7a
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-06 13:18:07 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

---
+++ trunk/app/controllers/CategoriasDeProdutos/SubCategoria.php
@@ -0,0 +1,78 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SubCategoria
+ *
+ * @author Luan
+ */
+class SubCategoria {
+    private $nome;
+    private $listaDeProdutos;
+
+     /** Construtor - define o codigo da SubCategoria
+     *
+     * @param <String> $nome
+     * @param <ArrayList<Produto>> $listaDeProdutos
+     */
+    public function  __construct($nome, $listaDeProdutos) {
+        $this->listaDeProdutos = array();
+
+    }
+
+     /** Retorna nome da SubCategoria
+     *
+     * @return <String> $nome
+     */
+    public function getNome(){
+
+    }
+
+     /** Define o novo nome da SubCategoria
+     *
+     * @param <String> $nome - Nome da SubCategoria
+     * @return void
+     */
+    public function setNome($nome){
+
+    }
+
+     /** Retorna a ArrayList de produtos da SubCategoria
+     *
+     * @return <ArrayList<Produto>> $listaDeProdutos
+     */
+    public function getProdutos(){
+
+    }
+
+     /** Faz uma busca na ArrayList de produtos da SubCategoria
+     *
+     * @param <String> $nomeProduto - Nome referente a um produto da SubCategoria
+     * @return <Produto> $produto - retorna um produto que possui o nome passado no parametro $nomeProduto
+     */
+    public function findProduto($nomeProduto){
+
+    }
+
+     /** Adiciona um Produto a ArrayList de produtos da SubCategoria
+     *
+     * @param <Produto> $produto - Novo produto a ser associada a SubCategoria
+     * @return void
+     */
+    public function adicionarProduto($produto){
+
+    }
+
+     /** Deleta um Produto da ArrayList de produtos da SubCategoria
+     *
+     * @param <Produto> $produto - Produto a ser retirado da ArrayList de produtos da SubCategoria
+     * @return void
+     */
+    public function deletarProduto($produto){
+
+    }
+}
+?>

2ee8357986aa2cc917303c3bf907455afe2070c9
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-06 14:01:08 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

--- trunk/app/controllers/Controle.php
+++ trunk/app/controllers/Controle.php
@@ -1,16 +1,28 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Classe Controle - Responsavel por juntar as partes de lógica de negócio \n
+ * e a visualização de sua aplicação.
+ *
+ * Os controllers receberão as requisições do usuário, farão algum processamento,\n
+ * farão contato com models, enviando os dados do usuário ou recebendo dados a \n
+ * serem mostrados em views.
+ *
  */
 
 /**
- * Description of PostsController
+ * Descrição da Classe Controle
  *
- * @author Rafael
+ * @author Rafael Tosta Santos <rafa.ecomp@gmail.com>
+ * @date 06/11/2010
+ * @version 1.0
+ * @group UEFS - Engenharia da Computacao
+ * @see Projeto de Graduação para a disciplina de Engenharia de Software 2010.2
+ * @link Repositorio <http://kiabosystem.googlecode.com/svn/trunk/app/controllers>
+ * @ref Controle
  */
 class Controle extends AppController{
     
-    //put your code here
+    
+
 }
 ?>

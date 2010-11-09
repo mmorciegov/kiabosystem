ce6952d12def3dc643d9bb8e66b668f8d273432b
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-09 03:05:34 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

--- trunk/app/controllers/Vendas/Produto.php
+++ trunk/app/controllers/Vendas/Produto.php
@@ -1,5 +1,6 @@
 <?php
-/* 
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -10,88 +11,94 @@
  * @author Luan
  */
 class Produto {
+
     private $codigo;
     private $nome;
     private $valor;
     private $categoria;
 
-     /** Construtor - define o codigo do produto
+    /** Construtor - define o codigo do produto
      *
      * @param <String> $codigo
      * @param <String> $nome
-      * @param <float> $valor
-      * @param <Categoria> $categoria
+     * @param <float> $valor
+     * @param <Categoria> $categoria
      */
-    public function  __construct($codigo, $nome, $valor, $categoria) {
-
+    public function __construct($codigo, $nome, $valor, $categoria) {
+        $this->codigo = $codigo;
+        $this->nome = $nome;
+        $this->valor = $valor;
+        $this->categoria = $categoria;
     }
 
-     /** Retorna o codigo do produto
+    /** Retorna o codigo do produto
      *
      * @return <String> $codigo
      */
-    public function getCodigo(){
-
+    public function getCodigo() {
+        return $this->codigo;
     }
 
-     /** Define o novo codigo do produto
+    /** Define o novo codigo do produto
      *
      * @param <String> $codigo - Codigo do produto
      * @return void
      */
-    public function setCodigo($codigo){
-
+    public function setCodigo($codigo) {
+        $this->codigo = $codigo;
     }
 
-     /** Retorna o nome do produto
+    /** Retorna o nome do produto
      *
      * @return <String> $nome
      */
-    public function getNome(){
-
+    public function getNome() {
+        return $this->nome;
     }
 
-     /** Define o novo nome do produto
+    /** Define o novo nome do produto
      *
      * @param <String> $nome - nome do produto
      * @return void
      */
-    public function setTelefone($nome){
-
+    public function setNome($nome) {
+        $this->nome = $nome;
     }
 
-     /** Retorna o valor do produto
+    /** Retorna o valor do produto
      *
      * @return <float> $valor
      */
-    public function getValor(){
-
+    public function getValor() {
+        return $this->valor;
     }
 
-     /** Define o novo valor do produto
+    /** Define o novo valor do produto
      *
      * @param <float> $valor - valor do produto
      * @return void
      */
-    public function setValor($valor){
-
+    public function setValor($valor) {
+        $this->valor = $valor;
     }
 
-     /** Retorna a categoria referente ao produto
+    /** Retorna a categoria referente ao produto
      *
      * @return <Categoria> $categoria
      */
-    public function getCategoria(){
-
+    public function getCategoria() {
+        return $this->categoria;
     }
 
-     /** Define a nova categoria referente ao produto
+    /** Define a nova categoria referente ao produto
      *
      * @param <Categoria> $categoria - categoria referente ao produto
      * @return void
      */
-    public function setCategoria($categoria){
-
+    public function setCategoria($categoria) {
+        $this->categoria = $categoria;
     }
+
 }
+
 ?>

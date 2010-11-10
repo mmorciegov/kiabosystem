1080a727527798b86e5ef041193b1a4e91222c33
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-10 03:12:12 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

---
+++ trunk/View/index.php
@@ -0,0 +1,22 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include '../Model/FormasDePagamentos/Boleto.php';
+        //include '../Model/Usuarios/Cliente.php';
+      
+      /*  $busca = new BuscasController();
+        $busca->buscaAvancada("computador", "01");*/
+       
+
+        ?>
+    </body>
+</html>

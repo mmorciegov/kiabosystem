4463f5a0f572e43f47693bfc0616290cec630686
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-10 02:43:34 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

---
+++ trunk/Controller/BuscasController.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BuscasController
+ *
+ * @author Rafael
+ */
+class BuscasController {
+    
+    /** Realiza a busca do produto a partir do nome do produto e a categoria
+     *
+     * @param <String> $nomeProduto
+     * @param <int> $categoria
+     */
+    public function buscaAvancada($nomeProduto, $categoria){
+
+    }
+}
+?>

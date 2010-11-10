5c5b695a27e0d300153c3482d3692f947460b769
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-10 03:28:55 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

--- trunk/Model/FormasDePagamentos/Cartao.php
+++ trunk/Model/FormasDePagamentos/Cartao.php
@@ -15,19 +15,47 @@
  * @link Repositorio <http://kiabosystem.googlecode.com/svn/trunk/app/controllers/FormasDePagamento>
  * @ref Cartao
  */
-
 include 'FormaPagamento.php';
 
-class Cartao extends FormaDePagamento{
+class Cartao extends FormaDePagamento {
+
+    /** Nome do titular do cartao
+     *
+     * @var <String> $nomeTitular
+     */
     private $nomeTitular;
+    /** Data de validade do cartao
+     *
+     * @var <String> $validade
+     */
     private $validade;
+    /** Numero do cartao
+     *
+     * @var <String> $numeroDoCartao
+     */
     private $numeroDoCartao;
+    /** Digito verificador do cartao
+     *
+     * @var <String> $digitoVerificador
+     */
     private $digitoVerificador;
+    /** CPF do titular do cartao
+     *
+     * @var <String> $cpfTitular
+     */
     private $cpfTitular;
+    /** Telefone do titular do cartao
+     *
+     * @var <String> $telefoneTitular
+     */
     private $telefoneTitular;
+    /** Senha do cartao
+     *
+     * @var <String> $senhaDoCartao
+     */
     private $senhaDoCartao;
 
-     /** Construtor - inicia as variaveis com os valores disponibilizados
+    /** Construtor - inicia as variaveis com os valores disponibilizados
      *
      * @param <int> $codigo
      * @param <String> $nomeTitular
@@ -38,125 +66,125 @@ class Cartao extends FormaDePagamento{
      * @param <String> $telefoneTitular
      * @param <String> $senhaDoCartao
      */
-    public function   __construct($codigo, $nomeTitular, $validade, $numeroDoCartao, $digitoVerificador, $cpfTitular, $telefoneTitular, $senhaDoCartao) {
+    public function __construct($codigo, $nomeTitular, $validade, $numeroDoCartao, $digitoVerificador, $cpfTitular, $telefoneTitular, $senhaDoCartao) {
         parent::__construct($codigo);
     }
 
-      /** Retorna o nome do titular do cartao
+    /** Retorna o nome do titular do cartao
      *
      * @return <String> $nomeTitular
      */
-    public function getNomeTitular(){
-
+    public function getNomeTitular() {
+        
     }
 
-     /** Define o novo nome do titular do cartao
+    /** Define o novo nome do titular do cartao
      *
      * @param <String> $nomeTitular - Nome do titular do cartao
      * @return void
      */
-    public function setNomeTitular($nomeTitular){
-
+    public function setNomeTitular($nomeTitular) {
+        
     }
 
-     /** Retorna a validade do cartao
+    /** Retorna a validade do cartao
      *
      * @return <String> $validade
      */
-    public function getValidade(){
-
+    public function getValidade() {
+        
     }
 
-     /** Define a nova validade do cartao
+    /** Define a nova validade do cartao
      *
      * @param <String> $validade - validade do cartao
      * @return void
      */
-    public function setValidade($validade){
-
+    public function setValidade($validade) {
+        
     }
 
-     /** Retorna o numero do cartao
+    /** Retorna o numero do cartao
      *
      * @return <String> $numeroDoCartao
      */
-    public function getNumero(){
-
+    public function getNumero() {
+        
     }
 
-     /** Define a nova validade do cartao
+    /** Define a nova validade do cartao
      *
      * @param <String> $numeroDoCartao - numero do cartao
      * @return void
      */
-    public function setNumero($numeroDoCartao){
-
+    public function setNumero($numeroDoCartao) {
+        
     }
 
-     /** Retorna o digito verificador do cartao
+    /** Retorna o digito verificador do cartao
      *
      * @return <String> $digitoVerificador
      */
-    public function getDigitoVerificador(){
-
+    public function getDigitoVerificador() {
+        
     }
 
-     /** Define o novo digito verificador do cartao
+    /** Define o novo digito verificador do cartao
      *
      * @param <String> $digitoVerificador - digito verificador do cartao
      * @return void
      */
-    public function setDigitoVerificador($digitoVerificador){
-
+    public function setDigitoVerificador($digitoVerificador) {
+        
     }
 
-     /** Retorna o telefone do titular do cartao
+    /** Retorna o telefone do titular do cartao
      *
      * @return <String> $telefoneTitular
      */
-    public function getTelefoneTitular(){
-
+    public function getTelefoneTitular() {
+        
     }
 
-     /** Define o novo telefone do titular do cartao
+    /** Define o novo telefone do titular do cartao
      *
      * @param <String> $telefoneTitular - Telefone do titular do cartao
      * @return void
      */
-    public function setTelefoneTitular($telefoneTitular){
-
+    public function setTelefoneTitular($telefoneTitular) {
+        
     }
 
-     /** Retorna a senha do cartao
+    /** Retorna a senha do cartao
      *
      * @return <String> $senhaDoCartao
      */
-    public function getSenha(){
-
+    public function getSenha() {
+        
     }
 
-     /** Define a nova senha do cartao
+    /** Define a nova senha do cartao
      *
      * @param <String> $senhaDoCartao - senha do cartao
      * @return void
      */
-    public function setSenha($senhaDoCartao){
-
+    public function setSenha($senhaDoCartao) {
+        
     }
 
-     /** Verifica se o CPF do titular do cartao é um CPF valido. Se for, retorna true
+    /** Verifica se o CPF do titular do cartao é um CPF valido. Se for, retorna true
      *
      * @return <boolean>
      */
-    public function verificarCPF(){
-
+    public function verificarCPF() {
+        
     }
 
-     /** Retorna o CPF do titular do cartao
+    /** Retorna o CPF do titular do cartao
      *
      * @return <String> $cpfTitular
      */
-    public function getCpfTitular(){
+    public function getCpfTitular() {
 
     }
 

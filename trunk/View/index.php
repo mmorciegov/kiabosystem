f35d3a9e806d800a0683dd91c6fd0122c747bfa9
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-11 17:00:18 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

--- trunk/View/index.php
+++ trunk/View/index.php
@@ -13,38 +13,6 @@ and open the template in the editor.
     <body>
         <?php
 
-
-       
-        
-      //  include '../Model/CategoriasDeProdutos/Categoria.php';
-      //  include '../Model/CategoriasDeProdutos/SubCategoria.php';
-        include '../Model/Vendas/Produto.php';
-        
-         //criando 50 caregorias
-       // $arrayDeCategorias = array();
-        //$arrayDeSubCategorias = array();
-        
-        $listaDeProdutos = array(1 => 'nomeP1', 2 => 'nomeP2');
-        
-      //  for ($i = 0; $i < 50; $i++) {
-          //  for ($j = 0; $j < 20; $j++){
-             //   for ($y = 0; $y < 5; $y++){
-            //        $newProduto = new Produto("132", "Produto".$y, 500, "categoria1");
-            //        $listaDeProdutos[''.$y] = $newProduto;
-                   
-           //        echo $listaDeProdutos[''.$y].'<br>';
-            //  }
-
-        //        $newSubCategoria = new SubCategoria("subCategoria".$j, $listaDeProdutos);
-             //   array_unshift($arrayDeSubCategorias,$newSubCategoria);
-         //   }
-         //   $newCategoria = new Categoria("Categoria".$i, $subCategorias);
-         //   array_unshift($arrayDeCategorias, $newCategoria);*/
-       // }
-
-//mostrando tudo teste
-       
-
         /*    //teste para o sistema de cep
           include '../Model/BancoDeCepDoBrasil/BuscaCEPOnline.php';
           $busca = new BuscaCEPOnline();

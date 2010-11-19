746470de686e5be10e850b567d1f755937a24f11
alen.brasil@gmail.com <alen.brasil@gmail.com@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-19 02:35:54 +0000

    reorganizacao do repositório

---
+++ trunk/web/app/models/subcategorias.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of subcategorias
+ *
+ * @author Alen
+ */
+class subcategorias extends AppModel{
+    //put your code here
+}
+?>
---
+++ trunk/web/app/views/users/register.htm.php
@@ -0,0 +1,13 @@
+<h2> Pagina de registro </h2>
+
+<?php
+
+
+echo $form->create();
+echo $form->input("username");
+echo $form->input("password");
+echo $form->close("Registrar");
+
+
+
+?>

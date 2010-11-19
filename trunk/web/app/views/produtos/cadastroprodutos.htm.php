6ff6ab159be596bbc9f139d9bd37f3885606c5d0
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-19 12:27:34 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

---
+++ trunk/web/app/views/produtos/cadastroprodutos.htm.php
@@ -0,0 +1,14 @@
+
+<?php
+
+echo $form->create();
+echo $form->input("cod");
+echo $form->input("subcategorias_idSubcategoria");
+echo $form->input("descricao");
+echo $form->input("nome");
+echo $form->input("quantidade");
+echo $form->input("preco");
+echo $form->input("promocional");
+
+echo $form->close("NovoProduto");
+?>

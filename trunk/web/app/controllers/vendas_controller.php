0d269d75ca36a4ec85727cce8503d56b23d52afa
alen.brasil@gmail.com <alen.brasil@gmail.com@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-23 17:15:15 +0000

    reorganizacao do repositorio

---
+++ trunk/web/app/controllers/vendas_controller.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of vendas_controller
+ *
+ * @author Alen
+ */
+class vendasController extends AppController {
+
+    function novaVenda(){
+    }
+}
+?>

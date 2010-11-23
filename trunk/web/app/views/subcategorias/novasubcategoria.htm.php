924529199c012c61894330975a604999ec216fe1
rafa.ecomp <rafa.ecomp@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-11-23 02:56:12 +0000

    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html>     <head>         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">         <title></title>     </head>     <body>         <?php         // put your code here         ?>     </body> </html>

--- trunk/web/app/views/subcategorias/novasubcategoria.htm.php
+++ trunk/web/app/views/subcategorias/novasubcategoria.htm.php
@@ -2,10 +2,10 @@
 <?php
 
 echo $form->create();
-echo $form->imput("idSubcategoria");
-echo $form->imput("categorias_idCategoria");
-echo $form->imput("descricao");
-echo $form->imput("nome");
+echo $form->input("idSubcategoria");
+echo $form->input("categorias_idCategoria");
+echo $form->input("descricao");
+echo $form->input("nome");
 
 echo $form->close("cadastra_subcategoria");
 ?>

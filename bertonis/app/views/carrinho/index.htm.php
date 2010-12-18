71b58844bcbf3c1d84587766ea1465f9925c8499
dededobem <dededobem@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-12-18 03:06:58 +0000

    modificações simples...

--- bertonis/app/views/carrinho/index.htm.php
+++ bertonis/app/views/carrinho/index.htm.php
@@ -1,6 +1,21 @@
+<div class="CADASTRO"><br><br><h1>CARRINHO!! </h1>
 
 
-<?php
+<br><h2><?php pr("Itens inseridos no carrinho:"); ?> </h2><br><br><br><br><br><br>
+
+<h3><p><?php
+//pr($nomesP);
+foreach ($nomesP as $pro){
+    echo "Produto: " . $pro["nome"]; ?> <?php echo "--> Quantidade: " . $QuantidadeP ?> <br>
+<?php }
+
+//print_r($carrinho);
 
 
-?>
+?> </h3>
+
+
+</div>
+
+
+

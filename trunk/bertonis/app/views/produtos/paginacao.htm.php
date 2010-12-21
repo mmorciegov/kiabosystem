105d97849f2e4d94e1fbe4a8638ea93a88bc81e1
dededobem <dededobem@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-12-21 06:44:34 +0000

    modificações simples...

--- trunk/bertonis/app/views/produtos/paginacao.htm.php
+++ trunk/bertonis/app/views/produtos/paginacao.htm.php
@@ -1,10 +1,15 @@
-<div class="frame_prods">
 
-<?php echo $produtos;?>  
-<div class="frame_pagig">     
+<div class="frame_prods">
+<table>
+<?php echo $produtos;?>
+</table> 
+<div class="frame_pagig">
+    
 <?php $pagination->model("Produtos");
        echo $pagination->previous("Anterior");
 	   echo " ".$pagination->next("Proximo"); 
-?></div>
+?>
+
+</div>
 </div>
 

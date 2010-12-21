281120a800e60aac9a67d020e5d600826555f816
dededobem <dededobem@c61fb205-ad2f-d7fb-52a2-1526ac488c22>
2010-12-21 06:41:16 +0000

    modificações simples...

--- bertonis/app/controllers/carrinho_controller.php
+++ bertonis/app/controllers/carrinho_controller.php
@@ -19,15 +19,23 @@ class CarrinhoController extends AppController {
         parent::__construct();
     }
 
-    public function index($nome = null) {
-        pr($nome);
+    public function index() {
+        
         //obtem o id da sessão
         $uid = $_SESSION["id_usuario"]; // atribuir a $uid o id da sessão do usuario
         $cart = $this->carrinho->fetch("SELECT DISTINCT (carrinho.product_id), carrinho.uid,carrinho.quantidade, produtos.nome
             FROM carrinho INNER JOIN produtos ON carrinho.product_id = produtos.cod
             WHERE carrinho.uid = '" . $uid . "'");
-		
-		$fim = "<a href='/bertonis/compras/index/".$uid."'>Finalizar Compra</a>";
+
+        //soma o valor total da compra do cliente
+        $somaTotal =  $this->carrinho->fetch("SELECT SUM(total_parcial) as SOMA, uid
+                        FROM carrinho WHERE uid = '" .$uid."' GROUP BY uid");
+
+        $this->set("valorFinal", $somaTotal);
+
+        foreach ($somaTotal as $soma){
+            $fim = "<a href='/bertonis/compras/index/".$uid. "/". $soma["SOMA"]."'>Finalizar Compra</a>";
+        }
 		
 		$this->set("link", $fim);
         
@@ -42,40 +50,48 @@ class CarrinhoController extends AppController {
         $this->set("produtos", $nomesProd);
         $this->set("total_preco", $this->carrinho->getTotalPrice($uid));
         $this->set("uid", $uid);
+
     }
 
     //finaliza a compra do cliente
     public function finalizaCompra($uid = null) {
         //armazenar os campos correspondentes da tabela de vendas
+        
     }
 
     // armazena um novo produto no carrinho
-    public function addProdutos($product_id = null) {
+    public function addProdutos($product_id = null, $preco = null) {
         
         //obtem o id da sessão
-        $uid = $_SESSION["id_usuario"];  // atribuir a $uid o id da sessão do usuario
-		
+        $uid = $_SESSION["id_usuario"];  // atribuir a $uid o id da sessão do usuario		
 		//procura o item no carrinho
-		$prod = $this->carrinho->fetch("SELECT id, uid, product_id, quantidade FROM carrinho WHERE product_id = '" .$product_id."'");
+		$prod = $this->carrinho->fetch("SELECT DISTINCT (carrinho.product_id), carrinho.id, carrinho.uid,
+                    carrinho.quantidade, produtos.preco FROM carrinho INNER JOIN produtos
+                    ON carrinho.product_id = produtos.cod WHERE product_id = '" .$product_id."'");                             
 		
 		if($prod!=null){// se o produto já estiver no carrinho
 			foreach($prod as $produto){
 				$produto["quantidade"]++;
-				$this->carrinho->delete($produto["id"]);
-				$this->carrinho->save(array("uid" => $produto["uid"],"product_id" => $produto["product_id"], quantidade => $produto["quantidade"]));
+				$this->carrinho->delete($produto["id"]);                               
+                                $totalParcial[] = $produto['quantidade'] * $produto['preco'];
 			}
-		}	else{//caso produto ainda Não esteja no carrinho
+                        $totalMult = array_sum($totalParcial);
+                        $this->carrinho->save(array("uid" => $produto["uid"],"product_id" => $produto["product_id"],
+                            "quantidade" => $produto["quantidade"], "total_parcial" => $totalMult));
+
+                }else{//caso produto ainda Não esteja no carrinho
 				//define o id do usuario e o id do produto para ser salvo
-        		$data = array(
+                        $data = array(
             		"uid" => $uid,
             		"product_id" => $product_id,
-					"quantidade" => 1
-            		//"nome" => $nome_produto
+			"quantidade" => 1,
+                        "total_parcial" => $preco            		
         		);
-
+                    
         		// sava os dados definidos        
         		$this->carrinho->save($data);
 		}//fim do else
+                
 		// redireciona a view
         $this->redirect("/produtos/paginacao");
 				
